144118ba7d999a58daaf7848c73098c100edb77f
srikanth9142 <srikanthtumpudi1999@gmail.com>
2019-06-03 21:23:36 +0530

    complete working game need to fix bonus chance it getting 2 bonus turns

--- main.js
+++ main.js
@@ -1,70 +1,193 @@
 var canvas = document.querySelector('canvas');
-var context = canvas.getContext("2d");
-
-//setting width and height of canvas
-canvas.height = (window.innerHeight)/3;
+canvas.height  = (window.innerHeight)/3;
 canvas.width = (window.innerWidth)/3;
 
-var mouseClicked = false,mouseReleased=true;
-//adding eventlisteners
-document.addEventListener('click',onmouseClick,false);
-document.addEventListener('mousemove',onmouseReleased,false);
 
+var context = canvas.getContext("2d");
 
-function onmouseClick(e){
-    mouseClicked = !mouseClicked;
-}
-//this function will plot a point at(p,q)
+context.fillStyle="blue";
+var totalLines=[];
+var dict={};
+var arrr=[];
+
+context.beginPath();
 function point(p,q){
     context.fillRect(p,q,3,3);
 }
-//plotting points
+//This is for plotting points.
 for(var j=40;j<=250;j+=30){
     for(var i=40;i<=250;i+=30){
         point(i,j);
     }
 }
-
-function onmouseReleased(e){
-    if(mouseClicked){
-        context.beginPath();
-        context.arc(e.clientX, e.clientY, 1.5, 0, Math.PI * 2, false);
-        context.lineWidth = 2;
-        context.strokeStyle = "black";
-        context.stroke();
+//context.fillStyle="orange";
+//context.fillText("2",55,55);
+//context.fillRect(70,70,-30,-30);
+//this function will store all initial array without lines.
+function storeData(){
+    for(var j=40;j<=250;j+=30){
+        for(var i=40;i<=250;i+=30){
+            arrr=[[j,i],[j+30,i]];
+            dict[arrr]=0;
+            totalLines.push(dict);
+            arr=[[j+30,i],[j,i]];
+            dict[arrr]=0;
+            totalLines.push(dict);
+        }
     }
+    console.log(totalLines[0]);
 }
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-  }
-var c1=0;
-var c2=0;
-function count1(){
-    c1+=1;
-    var st = "Score : "+c1;
-    document.getElementById("score1").innerHTML=st;
-}
-function count2(){
-    c2+=1;
-    var st = "Score : "+c2;
-    document.getElementById("score2").innerHTML=st;
-
-}
-function checkWinner(){
-    if(c1===0 && c2===0){
-        var st = "I can sense that you didn't made a box!! Play the game."
-        document.getElementById("windet").innerHTML=st;
+storeData();
+//This function will draw line and swap player turns.
+ function drawLine(arr,brr){
+    context.moveTo(arr[0],arr[1]);
+    context.lineTo(brr[0],brr[1]);
+    // if(Math.abs(arr[0]-brr[0])===30 || Math.abs(arr[1]-brr[1])===30){
+    var bg = [ [arr[0],arr[1]],[brr[0],brr[1]] ];
+    totalLines[0][bg]=1;
+    context.stroke();
+    bg = [ [brr[0],brr[1]],[arr[0],arr[1]] ];
+    totalLines[0][bg]=1;
+    console.log(totalLines[0]);
+    if(isLine(arr,brr)===1)
+    {
+        console.log("isline equals to 1")
+        if(turnFlag%2===0){
+            isBox();
+            if(!isBoxFlag){
+            console.log(" player 1isBoxFlag :"+isBoxFlag);
+            document.getElementById("turn2").innerHTML="Your turn";
+            document.getElementById("turn1").innerHTML="wait for your turn";
+            }
+        }
+        else{
+            isBox();
+            if(!isBoxFlag){
+            console.log("player 2isBoxFlag :"+isBoxFlag);
+            document.getElementById("turn1").innerHTML = "Your turn";
+            document.getElementById("turn2").innerHTML = "Wait for your turn";
+            }
+        }
+        turnFlag+=1;
     }
-    else if(c1>c2){
-        
-        var st = "Player1 wins! Heartly congratulations";
-        document.getElementById("windet").innerHTML = st;
-    }
-    else if(c2>c1){
-        var st = "Player2 wins! Heartly congratulations";
-        document.getElementById("windet").innerHTML = st;
-    }
+    //}
     else{
-        document.getElementById("windet").innerHTML = "Don\n't fight You both Win the game!";
+        document.getElementById("sc").innerHTML="Draw at points";
+        arr[0]=brr[0]=arr[1]=brr[1]=0;
     }
+ }
+$("#canvas").click(function(e){
+    getPosition(e);
+});
+var arr = new Array(2);
+var brr = new Array(2);
+var flag=-1,i=0;
+var altstr="";
+var turnFlag=2;
+//This function will takes coordinate and rounds it.
+function roundCoord(xcor){
+    var pg = xcor%10;
+    if(pg<5)
+        xcor=xcor-pg;
+    else 
+        xcor = xcor+(10-pg);
+    console.log("xcor:"+xcor);
+    return xcor;
+}
+ function getPosition(event){
+     if(flag<0){
+      arr[0] = roundCoord(event.clientX);
+      arr[1] = roundCoord(event.clientY);
+      i++;
+      console.log(i);
+      console.log(arr[0],arr[1]);
+    }
+      else{
+          brr[0] = roundCoord(event.clientX);
+          brr[1] = roundCoord(event.clientY);
+          i++;
+          console.log(i);
+          console.log(brr[0],brr[1]);
+      }
+      flag=flag*(-1);
+      //this function will call after 2 calls of getposition function.
+      if(i!=0 && i%2===0){
+        console.log("drawing line");
+        if((Math.abs(arr[0]-brr[0])===30&&Math.abs(arr[1]-brr[1])===0)||(Math.abs(arr[0]-brr[0])===0&&Math.abs(arr[1]-brr[1])===30)){
+        drawLine(arr,brr);
+        isBox(arr,brr);
+        document.getElementById("sc").innerHTML="msg";
+        }
+        else{
+            document.getElementById("sc").innerHTML="Draw at given points";
+        }
+   }
+ }
+ 
+ var cnt1=0,cnt2=0;
+ var isBoxFlag=false;
+//document.getElementById("sc").innerHTML=st;
+function isBox(){
+    var st="";
+    isBoxFlag=false;
+    for(var j=40;j<=250;j++){
+        for(var i=40;i<=250;i++){
+            var cond1 = totalLines[0][[ [i,j],[i+30,j]]]&&totalLines[0][[ [i+30,j],[i+30,j+30]]]&&totalLines[0][[ [i+30,j+30],[i,j+30]]]&&totalLines[0][[ [i,j+30],[i,j]]];
+            //totalLines[0][[ [i,j],[i+30,j]]]=totalLines[0][[ [i+30,j],[i+30,j+30]]]=totalLines[0][[ [i+30,j+30],[i,j+30]]]=totalLines[0][[ [i,j+30],[i,j]]]=0;
+            if(cond1){
+                totalLines[0][[ [i,j],[i+30,j]]]=totalLines[0][[ [i+30,j],[i+30,j+30]]]=totalLines[0][[ [i+30,j+30],[i,j+30]]]=totalLines[0][[ [i,j+30],[i,j]]]=0;
+                if(document.getElementById("turn1").innerHTML==="Your turn"){
+                    cnt1+=1;
+                    context.fillStyle="green";
+                    context.fillRect(i+30,j+30,-30,-30);
+                    isBoxFlag=true;
+                    //st+="player got :"+cnt;
+                    //document.getElementById("sc").innerHTML=st;
+                    console.log("score got : "+cnt1);
+                }
+                else{
+                    cnt2+=1;
+                    context.fillStyle="red";
+                    context.fillRect(i+30,j+30,-30,-30);
+                    isBoxFlag=true;
+                }
+
+            }
+        }
+    }
+    //st+="player got :"+cnt;
+    //document.getElementById("sc").innerHTML=st;
+     if(document.getElementById("turn1").innerHTML==="Your turn"){
+         document.getElementById("score1").innerHTML="score: "+cnt1;
+     }
+    else{
+         document.getElementById("score2").innerHTML="score: "+cnt2;
+     }
+     if(cnt1+cnt2===30){
+         if(cnt1>cnt2){
+             altstr="Game Over!! Player 1 win";
+             window.alert(altstr);
+         }
+         else if(cnt1<cnt2){
+             altstr ="Game Over!! Player 2 win";
+             window.alert(altstr);
+
+         }
+         else if(cnt1===cnt2){
+             altstr = "Well Play.Your Game Tie";
+             window.alert(altstr);
+         }
+         
+     }
+    
+}
+//isBox();
+function isLine(arr,brr){
+    var bg = [ [arr[0],arr[1]],[brr[0],brr[1]] ];
+    if(totalLines[0][bg]===1){
+        return 1;
+    }
+    else
+        return 0;
+    
 }
